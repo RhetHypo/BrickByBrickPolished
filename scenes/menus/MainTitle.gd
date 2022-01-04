71528094a253e1444e08b71e0352c0648b2fa67f
RhetHypo <65476977+RhetHypo@users.noreply.github.com>
2022-01-03 22:52:15 -0500

    Imported font, added main screen with animations

---
+++ scenes/menus/MainTitle.gd
@@ -0,0 +1,22 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$DelayTimer.start()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_DelayTimer_timeout():
+	$AnimationPlayer.play("Title")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Title":
+		$AnimationPlayer.play("Buttons")
